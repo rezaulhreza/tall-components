c007a8b30b92eaa52b1c5c3affb6b9c9a81b650d
Rezaul Hoque Reza <emailrezahere@gmail.com>
2022-12-04 21:09:19 +0000

    initializing MultiStepForm

--- Password Generator/PasswordGenerator.php
+++ Password Generator/PasswordGenerator.php
@@ -6,40 +6,5 @@ use Livewire\Component;
 
 class PasswordGenerator extends Component
 {
-    public $password;
-    public $password_confirmation;
-    public $visible = false;
-
-    public function render()
-    {
-        return view('livewire.password-generate');
-    }
-
-    public function generatePassword(): Void
-    {
-        $lowercase = range('a', 'z');
-        $uppercase = range('A', 'Z');
-        $digits = range(0,9);
-        $special = ['!', '@', '#', '$', '%', '^', '*'];
-        $chars = array_merge($lowercase, $uppercase, $digits, $special);
-        $length = env('PASSWORD_LENGTH', 8);
-
-        do {
-            $password = array();
-
-            for ($i = 0; $i <= $length; $i++) {
-                $int = rand(0, count($chars) - 1);
-                array_push($password, $chars[$int]);
-            }
-
-        } while (empty(array_intersect($special, $password)));
-
-        $this->setPasswords(implode('', $password));
-    }
-
-    public function setPasswords($value)
-    {
-        $this->password = $value;
-        $this->password_confirmation = $value;
-    }
+a
 }
