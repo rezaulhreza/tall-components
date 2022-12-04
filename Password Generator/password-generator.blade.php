f7c996268b504478277e2ab6993e5fc54c004059
Rezaul Hoque Reza <emailrezahere@gmail.com>
2022-12-04 05:01:53 +0000

    replace previous version- password generator

--- Password Generator/password-generator.blade.php
+++ Password Generator/password-generator.blade.php
@@ -1,34 +1,45 @@
-<div x-data="passwordGenerator()">
-  <label for="password">Password:</label>
-  <input x-model="password" type="text" id="password" readonly>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Parcel Sandbox</title>
+    <meta charset="UTF-8" />
+    <link
+      href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v3.10.5/dist/alpine.min.js"
+      defer></script>
+    
+  </head>
+<body>
 
-  <label for="length">Length:</label>
-  <input x-model.number="length" type="number" id="length">
+<div x-data="{
+  password: '',
+  length: 8,
+  generatePassword() {
+    let password = '';
+        for (let i = 0; i < this.length; i++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
 
-  <button @click="generatePassword">Generate Password</button>
-  <button @click="checkPasswordStrength">Check Password Strength</button>
+    this.password = password;
+  }
+}">
+      <form class="flex flex-col">
+        <div class="mt-5">
+        <label for="password">Password:</label>
+        <input type="text" x-model="password"/>
+        </div>
+        <label for="length">Password length:</label>
+         <input id="length" type="number" min="8" x-model="length">
+        <button type="submit" x-on:click.prevent="generatePassword()">Generate password</button>
+        
+      </form>
+
 </div>
 
+</body>
 
-<script>
-  window.passwordGenerator=passwordGenerator() {
-    return {
-      password: '',
-      length: 8,
-
-      generatePassword() {
-        // Generate a random password of the specified length
-        this.password = Math.random().toString(36).slice(-this.length);
-      },
-
-checkPasswordStrength() {
-        // Check if the password is weak
-        if (this.password.length < 8 || !this.password.match(/[A-Z]/) || !this.password.match(/[a-z]/) || !this.password.match(/\d/)) {
-          // Suggest a stronger password
-          this.password = 'Please use a stronger password, such as "P@55w0rd!"';
-        }
-      }
-    };
-  }
-</script>
+</html>
 
