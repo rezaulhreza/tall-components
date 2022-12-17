ccae2e93efbe25150378c4f512fa7eb5e5d07363
Rezaul H Reza <56729226+rezaulhreza@users.noreply.github.com>
2022-12-17 21:04:34 +0000

    Update chart.blade.php

--- Chart Component/chart.blade.php
+++ Chart Component/chart.blade.php
@@ -2,10 +2,10 @@
     chart = new Chart(document.getElementById('myChart').getContext('2d'), {
       type: 'line',
       data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],//$wire.labels
         datasets: [{
           label: 'My First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
+          data: [65, 59, 80, 81, 56, 55, 40],//$wire.sourceOfData
           backgroundColor: 'rgba(255, 99, 132, 0.2)',
           borderColor: 'rgba(255, 99, 132, 1)',
           borderWidth: 1
@@ -14,6 +14,8 @@
       options: {}
     });
   ">
+<h1>Dashboard</h1>
+<hr>
     <canvas id="myChart" width="400" height="400"></canvas>
   </div>
 
