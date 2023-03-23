5a77db780292ff2a271b2ea93653d0b184525999
Rezaul H Reza <56729226+rezaulhreza@users.noreply.github.com>
2023-03-23 14:53:25 +0000

    Create DesktopNotificator.vue

---
+++ Vue/DesktopNotificator.vue
@@ -0,0 +1,30 @@
+<template>
+  <button v-on:click="notify()">Click here to notify me</button>
+</template>
+
+<script>
+export default {
+  props: ["message", "title"],
+  methods: {
+    notify: function () {
+      if (Notification.permission !== "granted") {
+        Notification.requestPermission();
+      } else {
+        new Notification(this.title, {
+          body: this.message,
+        });
+      }
+    },
+  },
+};
+</script>
+
+
+//then use it in your app like:
+
+<desktop-notificator
+      message="This is the body"
+      title="This is a title"
+    ></desktop-notificator>
+    
+    //Make sure you import it!
